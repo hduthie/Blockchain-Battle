579176afc26bc71d6aee6dbc9ba0a54dae23be0e
hduthie <73220715+hduthie@users.noreply.github.com>
2022-10-26 16:18:29 +0100

    Changing the version of Solidity to match my compiler

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.21 ;
 
 library ConvertLib {
     function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount) {
